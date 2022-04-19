70e39716325f368c34402e1f223190f5223f32bb
Hoyeon Jang <cliched.0-hoedown@icloud.com>
2022-04-19 13:02:39 +0900

    revert for branch

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,4 @@
 # Selenium
-from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -7,9 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-# BeautifulSoup
-import requests
-from bs4 import BeautifulSoup
 # Type Annotation
 from typing import Dict, List
 # Data Visulization
@@ -42,18 +38,15 @@ class InvestingParser:
         self.chrome_options = Options()
         self.chrome_options.add_argument('--headless') 
         self.chrome_options.add_argument(f'--window-size=1920, 1080')
-    
-    def set_driver(self, url:str) -> None:
         self.driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
             options=self.chrome_options
             )
         self.driver.implicitly_wait(30)
-        self.driver.get(url)
 
     def get_tickers(self) -> Dict:
         # Setting Chrome Driver
-        self.set_driver('https://www.investing.com/equities/united-states')
+        self.driver.get('https://www.investing.com/equities/united-states')
         # Change ticker list to S&P 500 from Dow Jones
         button = self.driver.find_element(
             by=By.XPATH, 
@@ -69,12 +62,11 @@ class InvestingParser:
                 value='a'
             )
             tickers.append([info.text, info.get_attribute('href')])
-        self.driver.quit()
         return tickers
 
     def get_fundamental(self, url:str) -> List:
         # Setting Chrome Driver
-        self.set_driver(url)
+        self.driver.get(url)
         # Overview Page
         fundamental = []
         dl = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.TAG_NAME, 'dl')))
@@ -86,9 +78,8 @@ class InvestingParser:
         fundamental.append(float(low.replace(',', ''))) # 52wk lowest price
         fundamental.append(float(high.replace(',', ''))) # 52wk highest price
         fundamental.append(int(dds[13].text.replace(',', ''))) # Outstanding
-        self.driver.quit()
         # Financial Summary Page
-        self.set_driver(url+'-financial-summary')
+        self.driver.get(url+'-financial-summary')
         ep = EasyParse()
         # Total Revenue
         fundamental.append(ep.sum_items(self.driver.find_elements(
@@ -135,30 +126,30 @@ class InvestingParser:
             by=By.XPATH,
             value='/html/body/div[5]/section/div[12]/div[5]/table/tbody/tr[4]'
         )))
-        self.driver.quit()
         return fundamental
 
     def run(self) -> None:
-        # Parse Ticker list from investing.com
-        tickers = self.get_tickers()
-        # Save Ticker list to data/
-        df = pd.DataFrame(data=tickers, columns=['Co.', 'URL'])
-        df = df.set_index(['Co.'])
-        df.to_csv('./data/ticker_list.csv')
-        # Read Ticker list from data/
-        # maybe this is not essential
-        df = pd.read_csv('./data/ticker_list.csv', index_col=0)
+        try:
+            # Read Ticker list from data/
+            ticker_list = pd.read_csv('./data/ficker_list.csv', index_col=0)
+        except:
+            # Parse Ticker list from investing.com
+            tickers = self.get_tickers()
+            # Save Ticker list to data/
+            ticker_list = pd.DataFrame(data=tickers, columns=['Co.', 'URL'])
+            ticker_list = ticker.set_index(['Co.'])
+            ticker_list.to_csv('./data/ticker_list.csv')
         # Parse ticker's fundamental from each investing.com page
         columns = ['Low Price', 'High Price', 'Outstanding', 
                   'Total Revenue', 'Net Profit margin',
                   'Total Liabilities', 'Total Equity', 'LT Debt to Equity', 'Total Debt to Equity',
                   'Cash From Investing Activities', 'Cash From Financing Activities', 'Net Change in Cash']
         data = []
-        for ticker in df.index:
-            fundamental = self.get_fundamental(df.loc[ticker]['URL'])
+        for ticker in ticker_list.index:
+            fundamental = self.get_fundamental(ticker_list.loc[ticker]['URL'])
             data.append(fundamental)
-        df_fund = pd.DataFrame(data=data, columns=columns, index=df.index)
-        df_fund.to_csv('./data/fundamentals.csv')
+        ticker_fund = pd.DataFrame(data=data, columns=columns, index=ticker_list.index)
+        ticker_fund.to_csv('./data/fundamentals.csv')
         return
         
 if __name__ == '__main__':
