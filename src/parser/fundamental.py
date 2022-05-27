f1338f74392c2f4fe33e7c724680df9484682fa9
Hoyeon Jang <cliched.0-hoedown@icloud.com>
2022-05-27 11:52:11 +0900

    delete non-use package

--- src/parser/fundamental.py
+++ src/parser/fundamental.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import investpy
